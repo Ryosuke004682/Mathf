c39e2e2625d5ad66ccafa7b7185989f2772f1f33
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2022-11-04 02:54:09 +0900

    Y軸を中心としたTransform回転、Z軸を中心としたTransform回転の実装

--- MathfSample_Project/Assets/Script/Mathf_Logic/Transform/Transform2.cs
+++ MathfSample_Project/Assets/Script/Mathf_Logic/Transform/Transform2.cs
@@ -7,24 +7,22 @@ public class Transform2 : MonoBehaviour
     public Renderer rend;
     public Color color = Color.red;
     Vector3 position;
-
     Quaternion rotation;
 
     private void Start()
     {
+        rend = GetComponent<Renderer>();
+
         float X, Y, Z;
 
         X = Mathf.Abs(transform.position.x);
         Y = Mathf.Abs(transform.position.y);
         Z = Mathf.Abs(transform.position.z);
 
-        rend = GetComponent<Renderer>();
-
-        if ((X > Y) && (X > Z))
+        if( (X > Y) && (X > Z) )
         {
             color = Color.magenta;
         }
-        
         else
         {
             if(Y > Z)
@@ -42,9 +40,24 @@ public class Transform2 : MonoBehaviour
 
     }
 
-
-    private void Update()
+    private void FixedUpdate()
     {
         float angle = 2.0f * Mathf.PI * ((Time.time / 10.0f) % 1);
+
+        Matrix4x4 matrix = Matrix4x4.identity;
+        matrix.m00 =  Mathf.Cos(angle); 
+        matrix.m02 =  Mathf.Sin(angle);
+
+        matrix.m20 = -Mathf.Sin(angle);
+        matrix.m22 =  Mathf.Cos(angle);
+
+
+
+        transform.position = matrix * position;
+        transform.rotation = rotation;
+        transform.Rotate(0.0f,angle * 360.0f / (2.0f * Mathf.PI) , 0.0f , Space.World);
+
+
+        rend.material.color = color;
     }
 }
